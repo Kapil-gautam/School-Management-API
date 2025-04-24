921b5093f21d41c9589787250ceedd26278d6bd6
Kapil-gautam <kapilgautam.biet@gmail.com>
2025-04-24 20:27:03 +0530

    updated

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(express.json());
 app.use('/api', schoolRoutes);
 
+app.use('/',(req,res)=>{
+    res.send("Api is running...")
+})
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
